890195d370bf41a2618a05ffdb23044dd81e9fce
mtoasis <mtoasis22@gmail.com>
2017-11-20 21:31:15 -0800

    works in class

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,11 +12,27 @@ var database = firebase.database();
 var ref = database.ref();
 var connectedRef = firebase.database().ref(".info/connected");
 var connectionsRef = database.ref("/connections");
+var uncensored = false;
+
+$("#uncen_button").on("click",function(){
+	if (uncensored){
+		uncensored = false;
+		$(".xmark").css("display","block")
+		$("#uncen_button").css("background-color","green")
+	}
+	else if (!uncensored){
+	uncensored = true;
+	console.log(uncensored)
+	$("#uncen_button").css("background-color","red")
+	$(".xmark").css("display","none")
+}
+})
 
 connectedRef.on("value", function(snap){
 
   if (snap.val()) {
   	var count = snap.numChildren();
+
 
   	if (count<10){
   		count = "00"+count;
@@ -33,9 +49,17 @@ connectedRef.on("value", function(snap){
     var con = connectionsRef.push(updating_con);
 
 
+
+
     con.onDisconnect().remove();
 }
 });
+
+ref.child("connections").on("value", function(snap){
+
+	$("#view_number").text(snap.numChildren()+" people are online");
+
+})
 
 
 
@@ -75,11 +99,6 @@ $("#topic_text").text("Should people get married?")
 
 function time_now(){
 	var current_time = moment().format("hh:mm:ss A");	
-		// 	if (current_time == "00:00"){
-		// 		var topic = 
-
-		// 	$("#topic_text").text(topic)
-		// }
 		
 	$("#timer_display").text(current_time);
 }
@@ -91,9 +110,6 @@ function prettyDate2(time){
 }
 
 setInterval(time_now,1000)
-
-// var time_is = Date();
-// console.log(time_is)
 
 
 
@@ -128,12 +144,23 @@ var color_array = ["gradient_purple","gradient_yellow","gradient_green","gradien
 var rotation_array = ["rotate-left","rotate-right",""];
 
 
+$(document).on("keyup", "#input_text", function(){
+	var text_count = $("#input_text").val().length;
+	console.log(text_count)
+	if (text_count>=80){
+		$("#warning").text("You can't type more than 80 characters")
+		$()
+	}
+	else{
+		$("#warning").text("Character count: "+text_count)
+	}
+})
+
+
 
 ref.child("Posts").once("value",function(snap){
 
 	index = snap.numChildren();
-	// console.log("index: "+index)
-
 
 	snap.forEach(function(childsnap){
 
@@ -152,28 +179,33 @@ ref.child("Posts").once("value",function(snap){
 	var choice_rotate = Math.floor(Math.random()*3);
 
 	var post = $('<div id="'+i+'" class="postit '+color_array[choice_color]+' '+rotation_array[choice_rotate]+'">');
-	post.append($('<p>'+post_text+'</p>'));
-	var button = $('<button class="thumb_up">Like</button>');
-	post.append(button)
-	var button2 = $('<button class="thumb_down">Dislike</button>');
-	post.append(button2)
-	post.append('<br>')
-	var like_text = $('<span id="like'+i+'"">++</span>')
-	post.append(like_text)
-	post.append('<br>')
-	var dislike_text = $('<span id="dislike'+i+'"">--</span>')
+	var post_number = "#"+i;
+	post.append($('<p class="post_number">'+post_number+'</p>'));
+	post.append($('<p class="post_text">'+post_text+'</p>'));
+	var button = $('<button class="btn-floating green accent-3 thumb_up"><i class="material-icons">thumb_up</i>');
+    post.append(button)
+    var button2 = $('<button class="btn-floating red accent-3 thumb_down"><i class="material-icons">thumb_down</i>');
+    post.append(button2)
+    post.append('<br>')
+    var like_text = $('<p class="likes" id="like'+i+'"">++</p>')
+    post.append(like_text)
+    post.append('<br>')
+    var dislike_text = $('<p class="dislikes" id="dislike'+i+'"">--</p>')
 	post.append(dislike_text)
-	i=Number(i);
 
 	$("#post_area").append(post)
+	// like_refresh();
+	i=Number(i);
 
 })
+
 
 var new_index = 0;
 ref.child("Posts").on("value",function(snap){
 	new_index = snap.numChildren();
 
 })
+like_refresh();
 
 ref.child("Posts").limitToLast(1).on("child_added", function(snap){
 	
@@ -196,17 +228,20 @@ ref.child("Posts").limitToLast(1).on("child_added", function(snap){
 
 		var post = $('<div id="'+i+'" class="postit '+color_array[choice_color]+' '+rotation_array[choice_rotate]+'">');
 
-		post.append($('<p>'+post_text+'</p>'));
-		var button = $('<button class="thumb_up">Like</button>');
-		post.append(button)
-		var button2 = $('<button class="thumb_down">Dislike</button>');
-		post.append(button2)
-		post.append('<br>')
-		var like_text = $('<span id="like'+i+'"">++</span>')
-		post.append(like_text)
-		post.append('<br>')
-		var dislike_text = $('<span id="dislike'+i+'"">--</span>')
-		post.append(dislike_text)
+		var post_number = "#"+i;
+	post.append($('<p class="post_number">'+post_number+'</p>'));
+	post.append($('<p class="post_text">'+post_text+'</p>'));
+	var button = $('<button class="btn-floating green accent-3 thumb_up"><i class="material-icons">thumb_up</i>');
+    post.append(button)
+    var button2 = $('<button class="btn-floating red accent-3 thumb_down"><i class="material-icons">thumb_down</i>');
+    post.append(button2)
+    post.append('<br>')
+    var like_text = $('<p class="likes" id="like'+i+'"">++</p>')
+    post.append(like_text)
+    post.append('<br>')
+    var dislike_text = $('<p class="dislikes" id="dislike'+i+'"">--</p>')
+	post.append(dislike_text)
+
 
 		$("#post_area").append(post)
 		}
@@ -216,19 +251,17 @@ ref.child("Posts").limitToLast(1).on("child_added", function(snap){
 	})
 
 
-
 $("#submit_button").on("click",function(){
+
+	$("#bottom_nav_show").attr("disabled","disabled")
+	$("#submit_button").attr("disabled","disabled")
 
 	var input_text = $("#input_text").val().trim();
 
-	var posting = {
-		text: input_text,
-		like: 0,
-		dislike: 0
-	}
 	var index;
 	ref.child("Posts").once("value", function(snap){
 	var index = 1+snap.numChildren();
+
 
 	if (index<10){
 	index = String("00"+index);
@@ -241,25 +274,80 @@ if (index>=10 && index<100){
 	console.log("index is :"+index)
 	console.log(typeof index)
 }
+	var posting = {
+		text: input_text,
+		like: 0,
+		dislike: 0,
+		index: index
+	}
+
 	ref.child("Posts").child("post"+index).update(posting);
 	$("#input_text").val("");
 })
 })
 
-ref.on("value",function(){
+
+
+function xmark(){
+	ref.child("Posts").once("value",function(snap){
+		var index = snap.numChildren();
+		var i=0;
+			snap.forEach(function(childsnap){
+
+			if (!uncensored){
+
+			// 	for (var i=0;i<index;i++){
+
+
+			if (i<9){
+				var target = "#00"+Number(i+1);
+				}
+			else if(i>=9 && i<99) {
+				var target = "#0"+Number(i+1);
+			}
+			else {
+				var target = "#"+Number(i+1);
+			}
+		
+				var dislikes = childsnap.val().dislike
+
+				if (dislikes>7){
+					// console.log("working")
+					console.log(target)
+					// $(target).css("display","block")
+
+				var x_mark_img = $('<img class="xmark" src="assets/images/xmark.png">');
+
+				$(target).append(x_mark_img)			
+
+				}
+
+			i +=1;
+		}
+
+		})
+			})
+		}
+
+function like_refresh(){
+	xmark()
+	console.log("refreshing likes")
+	ref.on("value",function(){
 	ref.child("Posts").on("value",function(snap){
 
 
 		var index = snap.numChildren();
 		var array_like = [];
 		var array_dislike = [];
+		// var dislikes
 		snap.forEach(function(childsnap){
 
+
 			var likes = childsnap.val().like;
-			var dislikes = childsnap.val().dislike;	
+			var dislikes = childsnap.val().dislike;
+
 			array_like.push(likes);
 			array_dislike.push(dislikes);
-
 		})
 			for (var i=0;i<index;i++){
 
@@ -267,28 +355,33 @@ ref.on("value",function(){
 				var target = "#like00"+Number(i+1);
 				var target2 = "#dislike00"+Number(i+1);
 				}
-			else {
+			else if(i>=9 && i<99) {
 				var target = "#like0"+Number(i+1);
 				var target2 = "#dislike0"+Number(i+1);
+			}
+			else {
+				var target = "#like"+Number(i+1);
+				var target2 = "#dislike"+Number(i+1);
 			}
 
 			$(target).text("Like: "+ array_like[i])
 			$(target2).text("Dislike: "+array_dislike[i])
 		}
 
-		console.log(array_like.length)
 
-	})
+			// console.log(array_like.length)
+
 });
+})
+}
 
-
+setInterval(like_refresh,3000)
 
 
 
 $(document).on("click",".thumb_up", function(){
 	var id = $(this).parent().attr("id");
-	$(this).attr("disabled")
-	// console.log(id);
+	$(this).attr({"disabled": "disabled"})
 	var like;	
 	ref.child("Posts").child("post"+id).on("value", function(snap){
 		like = snap.val().like;
@@ -300,6 +393,7 @@ $(document).on("click",".thumb_up", function(){
 })
 
 $(document).on("click",".thumb_down", function(){
+	$(this).attr({"disabled": "disabled"})
 	var id = $(this).parent().attr("id");
 	var dislike;	
 	ref.child("Posts").child("post"+id).on("value", function(snap){
@@ -311,6 +405,126 @@ $(document).on("click",".thumb_down", function(){
 	ref.child("Posts").child("post"+id).update(updating);
 })
 
+
+$("#order_by_like").on("click",function(){
+	$("#post_area").empty();
+
+	ref.child("Posts").orderByChild("like").on("child_added",function(snap){
+		console.log(snap.val())
+		var index = snap.numChildren();
+
+	var post_text = snap.val().text;
+
+	var choice_color = Math.floor(Math.random()*4);
+	var choice_rotate = Math.floor(Math.random()*3);
+	var i = snap.val().index;
+
+	var post = $('<div id="'+i+'" class="postit '+color_array[choice_color]+' '+rotation_array[choice_rotate]+'">');
+	var post_number = "#"+i;
+	post.append($('<p class="post_number">'+post_number+'</p>'));
+	post.append($('<p class="post_text">'+post_text+'</p>'));
+	var button = $('<button class="thumb_up">Like</button>');
+	post.append(button)
+	var button2 = $('<button class="thumb_down">Dislike</button>');
+	post.append(button2)
+	post.append('<br>')
+	var like_text = $('<span id="like'+i+'"">++</span>')
+	post.append(like_text)
+	post.append('<br>')
+	var dislike_text = $('<span id="dislike'+i+'"">--</span>')
+	post.append(dislike_text)
+
+	$("#post_area").prepend(post)
+	like_refresh();
+
+})
+
+	})
+
+$("#order_by_dislike").on("click",function(){
+	$("#post_area").empty();
+
+	ref.child("Posts").orderByChild("dislike").on("child_added",function(snap){
+		console.log(snap.val())
+		var index = snap.numChildren();
+
+	var post_text = snap.val().text;
+
+	var choice_color = Math.floor(Math.random()*4);
+	var choice_rotate = Math.floor(Math.random()*3);
+	var i = snap.val().index;
+
+	var post = $('<div id="'+i+'" class="postit '+color_array[choice_color]+' '+rotation_array[choice_rotate]+'">');
+	var post_number = "#"+i;
+	post.append($('<p class="post_number">'+post_number+'</p>'));
+	post.append($('<p class="post_text">'+post_text+'</p>'));
+	var button = $('<button class="thumb_up">Like</button>');
+	post.append(button)
+	var button2 = $('<button class="thumb_down">Dislike</button>');
+	post.append(button2)
+	post.append('<br>')
+	var like_text = $('<span id="like'+i+'"">++</span>')
+	post.append(like_text)
+	post.append('<br>')
+	var dislike_text = $('<span id="dislike'+i+'"">--</span>')
+	post.append(dislike_text)
+
+	$("#post_area").prepend(post)
+
+	like_refresh();
+
+})
+
+	})
+
+$("#order_by_date").on("click",function(){
+	$("#post_area").empty();
+	ref.child("Posts").once("value",function(snap){	
+	// index = snap.numChildren();
+	var i=0;
+	snap.forEach(function(childsnap){
+
+	i +=1;
+
+	if (i<10){
+		i="00"+i;
+	}
+	if (i>=10 && i<100){
+		i="0"+i;
+	}
+
+	var post_text = childsnap.val().text;
+
+	var choice_color = Math.floor(Math.random()*4);
+	var choice_rotate = Math.floor(Math.random()*3);
+
+	var post = $('<div id="'+i+'" class="postit '+color_array[choice_color]+' '+rotation_array[choice_rotate]+'">');
+	var post_number = "#"+i;
+	post.append($('<p class="post_number">'+post_number+'</p>'));
+	post.append($('<p class="post_text">'+post_text+'</p>'));
+	var button = $('<button class="thumb_up">Like</button>');
+	post.append(button)
+	var button2 = $('<button class="thumb_down">Dislike</button>');
+	post.append(button2)
+	post.append('<br>')
+	var like_text = $('<span id="like'+i+'"">++</span>')
+	post.append(like_text)
+	post.append('<br>')
+	var dislike_text = $('<span id="dislike'+i+'"">--</span>')
+	post.append(dislike_text)
+	i=Number(i);
+
+	$("#post_area").append(post)
+	like_refresh();
+
+})
+})
+})
+
+
+
+
+
 $('.fixed_bottom').hide();
 $('.fixed_right').hide();
 
@@ -319,9 +533,9 @@ $(".close-button").click(function(){
   $('.fixed_bottom').hide();
 });
 
-$("#bottom_nav_show").click(function(){
-  $('.fixed_bottom').show();
-});
+// $("#post_button").click(function(){
+//   $('.fixed_bottom').show();
+// });
 
 //LEADER BOARD SLIDE OUT
 $('#leaderboard_slide_out').click(function() {
@@ -331,3 +545,124 @@ $('#leaderboard_slide_out').click(function() {
 $("#leaderboard_show").click(function(){
   $('.fixed_right').show();
 });
+
+//MODAL SUBMIT POP-UP
+$('.modal').modal();
+//LEADERBOARD SLIDE-OUT
+$('.button-collapse').sideNav({
+    menuWidth: 300,
+    closeOnClick: true,
+    edge: 'right'
+});
+
+// $("#post_button").on("click", function(){
+
+// 	var back_div = $('<div class="back_div">');
+// 	var body = $(".outer");
+// 	body.append(back_div)
+
+// })
+
+
+// //zhong part
+
+
+// // begin of upload image
+// // -------------------------------------------------------------------
+//    function previewFile(){
+//        var preview = document.querySelector('img'); //selects the query named img
+//        var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+//        var reader  = new FileReader();
+//        reader.onloadend = function () {
+//            preview.src = reader.result;
+//        }
+//        if (file) {
+//            reader.readAsDataURL(file); //reads the data as a URL
+//        } else {
+//            preview.src = "";
+//        }
+//   }
+//   previewFile();  //calls the function named previewFile()
+// // -------------------------------------------------------------------
+// // end of upload image
+
+
+// // begin of get modal
+// // -------------------------------------------------------------------
+//  var modal = document.getElementById('myModal');
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }// -------------------------------------------------------------------
+// // end of get modal
+
+
+// $(document).ready(function(){
+// //hide div
+// $("#display").hide()
+
+// // get element
+// var uploader = document.getElementById("uploader");
+// var fileButton = document.getElementById("fileButton");
+// var text_input = "";
+
+// // listen for file selection
+// fileButton.addEventListener("change",function(e){
+//     var file = e.target.files[0];
+//   //create a storage
+//   var storageRef = firebase.storage().ref("user_gifs/"+file.name);
+//   //upload file
+//   var task = storageRef.put(file);
+//   //update progress bar
+//   task.on("state_changed",
+//     function progress(snapshot){
+//       var percentage = (snapshot.bytesTransferred / snapshot.totalBytes)*100;
+//       uploader.value = percentage;
+//     },
+//     function error(err){
+//     },
+//     function complete(){
+//       // Upload completed successfully, now we can get the download URL
+
+// var postKey = firebase.database().ref('Posts/').push().key;
+// var downloadURL = task.snapshot.downloadURL;
+   
+// //click function
+//    $("#submit").on("click",function(){
+//    event.preventDefault();
+//   text_input = $("#text_input").val().trim();
+//   var updates = {};
+//   var postData = {
+//     url: downloadURL,
+//     caption:text_input
+//       };
+//   updates['/Posts/'+postKey] = postData;
+//   firebase.database().ref().update(updates);
+//   console.log(downloadURL);
+//   console.log(text_input);
+
+//   var displayDiv;
+//   var imgDiv = '<img src="'+downloadURL+'" width="300px">';
+//   var textDiv = '<p>'+text_input+'</p>'
+//   displayDiv = '<div>' + imgDiv + textDiv+'</div>'
+//   $("#display").show();
+//   $("#display").html(displayDiv);
+
+//   })
+// })
+// })
+// })
